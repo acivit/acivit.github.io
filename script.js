954542195fca23fe491d1454bc9131c81e0c29aa
acivit <acivit@iri.upc.edu>
2024-02-16 12:09:04 +0100

    Changes to try to make citations work

--- script.js
+++ script.js
@@ -1,71 +1,59 @@
-const slider = document.getElementById('timeline-slider');
-const events = document.querySelectorAll('.event');
-
-slider.addEventListener('input', () => {
-  const value = slider.value;
-  events.forEach((event, index) => {
-    const position = (index + 1) * 100 / (events.length + 1);
-    event.style.left = `${position + value}%`;
-  });
-});
-
-
-function openCitation(title) {
-  var citationText;
-  // Set the citation text based on the title
-  if (title === "Civit_hri24") {
-    citationText = `@inproceedings{Civit_hri24,
-      title = {Introducing Social Robots to Assess Frailty in Older Adults},
-      booktitle = {Proceedings of the 2024 {ACM}/{IEEE} {International} {Conference} on {Human}-{Robot} {Interaction}},
-      author = {Civit, Aniol
-                  and Andriella, Antonio
-                  and Barrué, Cristian
-                  and Antonio, Maite
-                  and Boqué, Concepción
-                  and Alenyà, Guillem},
-      publisher = {Association for Computing Machinery},
-      month = mar,
-      year = {2024},
-      pages = {XXX--XXX}
-    }`;
-  } else {
-    citationText = "No citation available.";
-  }
-  // Open a small window with plain text citation
-  window.open('', 'Citation', 'width=400,height=200').document.write('<pre>' + citationText + '</pre>');
-};
+// function openCitation(title) {
+//   var citationText;
+//   // Set the citation text based on the title
+//   if (title === "Civit_hri24") {
+//     citationText = `@inproceedings{Civit_hri24,
+//       title = {Introducing Social Robots to Assess Frailty in Older Adults},
+//       booktitle = {Proceedings of the 2024 {ACM}/{IEEE} {International} {Conference} on {Human}-{Robot} {Interaction}},
+//       author = {Civit, Aniol
+//                   and Andriella, Antonio
+//                   and Barrué, Cristian
+//                   and Antonio, Maite
+//                   and Boqué, Concepción
+//                   and Alenyà, Guillem},
+//       publisher = {Association for Computing Machinery},
+//       month = mar,
+//       year = {2024},
+//       pages = {XXX--XXX}
+//     }`;
+//   } else {
+//     citationText = "No citation available.";
+//   }
+//   // Open a small window with plain text citation
+//   window.open('', 'Citation', 'width=400,height=200').document.write('<pre>' + citationText + '</pre>');
+// };
 
 
-function openCitation_modal(identifier) {
-  var citationText;
-  // Set the citation text based on the identifier
-  if (identifier === "Civit_hri24") {
-    citationText = `@inproceedings{Civit_hri24,
-                    title = {Introducing Social Robots to Assess Frailty in Older Adults},
-                    booktitle = {Proceedings of the 2024 {ACM}/{IEEE} {International} {Conference} on {Human}-{Robot} {Interaction}},
-                    author = {Civit, Aniol
-                              and Andriella, Antonio
-                              and Barrué, Cristian
-                              and Antonio, Maite
-                              and Boqué, Concepción
-                              and Alenyà, Guillem},
-                    publisher = {Association for Computing Machinery},
-                    month = mar,
-                    year = {2024},
-                    pages = {XXX--XXX}
-                    }`;
-  } else {
-    citationText = "No citation available.";
-  }
-  // Display the modal with citation content
-  var modal = document.getElementById("citationModal");
-  var citationContent = document.getElementById("citationContent");
-  citationContent.textContent = citationText;
-  modal.style.display = "block";
-};
+// function openCitation_modal(identifier) {
+//   var citationText;
+//   // Set the citation text based on the identifier
+//   if (identifier === "Civit_hri24") {
+//     citationText = `@inproceedings{Civit_hri24,
+//                     title = {Introducing Social Robots to Assess Frailty in Older Adults},
+//                     booktitle = {Proceedings of the 2024 {ACM}/{IEEE} {International} {Conference} on {Human}-{Robot} {Interaction}},
+//                     author = {Civit, Aniol
+//                               and Andriella, Antonio
+//                               and Barrué, Cristian
+//                               and Antonio, Maite
+//                               and Boqué, Concepción
+//                               and Alenyà, Guillem},
+//                     publisher = {Association for Computing Machinery},
+//                     month = mar,
+//                     year = {2024},
+//                     pages = {XXX--XXX}
+//                     }`;
+//   } else {
+//     citationText = "No citation available.";
+//   }
+//   // Display the modal with citation content
+//   var modal = document.getElementById("citationModal");
+//   var citationContent = document.getElementById("citationContent");
+//   citationContent.textContent = citationText;
+//   modal.style.display = "block";
+// };
 
-function closeModal() {
-  // Close the modal
-  var modal = document.getElementById("citationModal");
-  modal.style.display = "none";
-};
+// function closeModal() {
+//   // Close the modal
+//   var modal = document.getElementById("citationModal");
+//   modal.style.display = "none";
+// };
