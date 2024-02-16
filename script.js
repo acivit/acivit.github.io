588046eeca165a153cd0ffd8563120d8ab186164
acivit <acivit@iri.upc.edu>
2024-02-16 10:36:05 +0100

    Adding publication

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ slider.addEventListener('input', () => {
     event.style.left = `${position + value}%`;
   });
 });
+
+
+function openCitation(title) {
+  var citationText;
+  // Set the citation text based on the title
+  if (title === "Civit_hri24") {
+    citationText = `@inproceedings{Civit_hri24,
+      title = {Introducing Social Robots to Assess Frailty in Older Adults},
+      booktitle = {Proceedings of the 2024 {ACM}/{IEEE} {International} {Conference} on {Human}-{Robot} {Interaction}},
+      author = {Civit, Aniol
+                  and Andriella, Antonio
+                  and Barrué, Cristian
+                  and Antonio, Maite
+                  and Boqué, Concepción
+                  and Alenyà, Guillem},
+      publisher = {Association for Computing Machinery},
+      month = mar,
+      year = {2024},
+      pages = {XXX--XXX}
+    }`;
+  } else {
+    citationText = "No citation available.";
+  }
+  // Open a small window with plain text citation
+  window.open('', 'Citation', 'width=400,height=200').document.write('<pre>' + citationText + '</pre>');
+}
